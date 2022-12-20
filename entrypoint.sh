bbcc46a64cbde93a530a3fe312e96f687b52cc8c
Francisco Martín Rico <fmrico@gmail.com>
2022-12-20 08:15:36 +0100

    Create issue
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -91,7 +91,6 @@ then
   echo "Complete message: " >> bodyfile 
   echo "${MERGE_RESULT}" >> bodyfile
   cat bodyfile
-  echo ${GITHUB_TOKEN} > gt
-  gh auth login --git-protocol https --hostname GitHub.com --with-token < gt
-  gh issue create --repo https://github.com/${GITHUB_REPOSITORY}.git --title "Fix conflict in $CONFLICTS" --body-file bodyfile
+  # gh auth login --git-protocol https --hostname GitHub.com --with-token < gt
+  GH_TOKEN=${GITHUB_TOKEN} gh issue create --repo https://github.com/${GITHUB_REPOSITORY}.git --title "Fix conflict in $CONFLICTS" --body-file bodyfile
 fi
